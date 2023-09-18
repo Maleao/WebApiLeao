f83b11f6e4c6809917ec61043b88af021c826b7d
Maleao <mtdevjf@gmail.com>
2023-09-18 19:05:23 -0300

    Implementando projeto *.Repository e Interface BaseGeneric

---
+++ WebApiLeao.Repository/BaseGeneric/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Data.Contexto;
+
+namespace WebApiLeao.Repository.BaseGeneric
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly DataContext _dataContext;
+
+        public BaseRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task Delete(int Id)
+        {
+            var entity = await GetById(Id);
+            _dataContext.Set<T>().Remove(entity);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _dataContext.Set<T>().AsNoTracking().ToListAsync(); //asNoTraking importante colocar para a consulta ficar mais rápida
+        }
+
+        public async Task<T> GetById(int Id)
+        {
+            return await _dataContext.Set<T>().FindAsync(x => x.Id == Id);
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _dataContext.Set<T>().AddAsync(entity);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task Update(int Id, T entity)
+        {
+            _dataContext.Set<T>().Update(entity);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
