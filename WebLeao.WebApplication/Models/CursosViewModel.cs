ba501c896fe1e5102c94fe67ac73dcf605d0009f
Maleao <mtdevjf@gmail.com>
2023-09-21 12:06:40 -0300

    Criado CursosController e CursosViewModel

---
+++ WebLeao.WebApplication/Models/CursosViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebLeao.WebApplication.Models
+{
+    public class CursosViewModel
+    {
+        public int Id { get; set; }
+        public DateTime DataRegistro { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public Decimal Valor { get; set; }
+    }
+}
